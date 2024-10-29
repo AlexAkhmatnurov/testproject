f0ea00c47b109484e899f6c792bdc628a9625ece
AlexAkhmatnurov <146396856+AlexAkhmatnurov@users.noreply.github.com>
2024-10-29 00:37:56 +0000

    block-CTA

--- app/public/wp-content/themes/cesca/blocks/cta/cta.php
+++ app/public/wp-content/themes/cesca/blocks/cta/cta.php
@@ -1,9 +1,53 @@
-<div class="cta-grid">
-    <div class="cta">
-        <h2 class="primary-dark-text">Call +44 7403 750039 for a free 20-minute initial discussion</h2>
-        <p class="main-text">Or email <a class="primary-main-text" href="mailto:myqueryto@cescauk.org">myqueryto@cescauk.org</a> to explore you situation and have an answer to your questions</p>
+<?php
+/**
+ * CTA Block template.
+ *
+ * @param array $block The block settings and attributes.
+ */
+
+// Load values and assign defaults.
+$main_call_text         = !empty(get_field( 'main_call_text' )) ? get_field( 'main_call_text' ) : 'Put the main call text here...';
+$secondary_call_text    = !empty(get_field( 'secondary_call_text' )) ? get_field( 'secondary_call_text' ) : 'Put the secondary call text here...';
+$image                  = get_field( 'image' );
+$background_color       = get_field( 'background_color' );
+$main_text_color        = get_field( 'main_text_color' );
+$secondary_text_color   = get_field( 'secondary_text_color' );
+
+// Support custom "anchor" values.
+$anchor = '';
+if ( ! empty( $block['anchor'] ) ) {
+    $anchor = 'id="' . esc_attr( $block['anchor'] ) . '" ';
+}
+
+// Create class attribute allowing for custom "className" and "align" values.
+$class_name = 'cta';
+if ( ! empty( $block['className'] ) ) {
+    $class_name .= ' ' . $block['className'];
+}
+if ( ! empty( $block['align'] ) ) {
+    $class_name .= ' align' . $block['align'];
+}
+if ( $background_color || $main_text_color  || $secondary_text_color) {
+    $class_name .= ' has-custom-acf-colors';
+}
+
+?>
+<div <?php echo esc_attr( $anchor ); ?>class="<?php echo esc_attr( $class_name ); ?>" style="background-color: <?php echo esc_attr( $background_color ); ?>">
+    <div class="cta-grid">
+        <div class="cta-text">
+            <h2 style="color: <?php echo esc_attr( $main_text_color ); ?>">
+                <?php echo esc_html( $main_call_text ); ?>
+            </h2>
+            <p style="color: <?php echo esc_attr( $secondary_text_color ); ?>">
+                <?php echo esc_html( $secondary_call_text ); ?>
+            </p>
+        </div>
+        <div>
+            <?php if ( $image ) : ?>
+                <?php echo wp_get_attachment_image( $image['ID'], 'full', '', array( 'class' => 'cta__img' ) ); ?>
+            <?php else : ?>    
+                <img class="cta-image" src="<?php echo get_template_directory_uri()."/images/cta.png" ?>" alt="@">
+            <?php endif; ?>      
+        </div>
     </div>
-    <div>
-        <img class="cta-image" src="<?php echo get_template_directory_uri()."/images/cta.png" ?>" alt="@">
-    </div>
-</div>    
+</div>         
